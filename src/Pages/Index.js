87fcaf209c20eae8af1252b80b6ed020c78f956b
Marangely Rosas <Ladykaliber22@gmail.com>
2023-05-18 01:09:52 -0400

    completed Index and Show Pages functionality

---
+++ src/Pages/Index.js
@@ -0,0 +1,12 @@
+import Jobs from "../Components/Jobs";
+
+function Index() {
+  return (
+    <div className="Index">
+      <h2>Index</h2>
+      <Jobs />
+    </div>
+  );
+}
+
+export default Index;
---
+++ src/Pages/Show.js
@@ -0,0 +1,12 @@
+import JobDetails from "../Components/JobDetails";
+
+function Show() {
+  return (
+    <div className="Show">
+      <h2>Show</h2>
+      <JobDetails />
+    </div>
+  );
+}
+
+export default Show;
