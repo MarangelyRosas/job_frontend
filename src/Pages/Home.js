e7813e73fb47cd48ea69b029e1e55b35d6d82f68
Marangely Rosas <Ladykaliber22@gmail.com>
2023-05-22 02:01:12 -0400

    App description

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,8 +1,10 @@
 function Home() {
     return (
       <div>
-        <h2>Welcome to Marangely's FSW Developer Job Board</h2>
+        <h2>Welcome to Marangely's Software Engineering Job Board</h2>
         <h3>Here you can browse for any Dev Job openings, whether you're interested in Front-end, Back-end, or Full-Stack Positions</h3>
+        <h4>About CodeCareer: </h4>
+        <p>This Software Engineering Job Board is a specialized web application designed by me (Marangely - an inspired junior software engineering student currently on the road to becoming an above exceptional FSW Developer!) for software engineers and employers in the software development industry. The app serves as a platform for employers to post job listings tailored to software engineering roles, including job titles, descriptions, requirements, and locations. Software engineers can search for relevant job opportunities based on roles or positions they have most experience level in or even a preferred/position of choice. The app provides user authentication for both employers and job seekers, allowing employers to manage their company information and job listings while enabling job seekers to update their skills and track their applications. With email notifications and seamless communication features, this Software Engineering Job Board simplifies the job search process for software engineers and facilitates efficient hiring for employers in the software engineering field.</p>
       </div>
     );
   }
